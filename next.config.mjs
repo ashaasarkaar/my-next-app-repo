74b7de4bbfc47a9d80e9bf6ed91e19dfd68106d1
Salaatsarkar <salaatsarkar0501@gmail.com>
2026-09-12 09:21:15 +0600

    Image Optimization in Next.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,28 @@
 /** @type {import('next').NextConfig} */
+// https://images.unsplash.com/photo-1723296637578-95124ce6024b?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D
+
+// 'https://i.ibb.co.com/FbNYdFM5/The-Dating-Play-Book-For-Men-Book.png'
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '**',
+        search: '',
+      },
+       {
+        protocol: 'https',
+        hostname: 'i.ibb.co.com',
+        port: '',
+        pathname: '/FbNYdFM5/**',
+        search: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,17 @@
+import Image from 'next/image';
 import React from 'react';
+import virat from '../../assets/images/virat-kolijpg.jpg'
 
 const AboutPage = () => {
     return (
         <div>
             <h1 className='text-amber-400 text-7xl'>This is About Section</h1>
+            <div className='grid grid-cols-4 gap-4'>
+                <Image src='/african.jpg' alt='africal lady' width='500' height='300'></Image>
+            <Image src={virat} alt='virat' width='500' height='300'></Image>
+            <Image src='https://images.unsplash.com/photo-1723296637578-95124ce6024b' alt='Egg line' height='300' width='500'></Image>
+            <Image src='https://i.ibb.co.com/FbNYdFM5/The-Dating-Play-Book-For-Men-Book.png' alt='book image' width='500' height='300'></Image>
+            </div>
         </div>
     );
 };
